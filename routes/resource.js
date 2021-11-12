5c85838ffe4a096b6503c6c49f3ef36644118106
mounishaS545235 <89499369+mounishaS545235@users.noreply.github.com>
2021-11-12 12:05:02 -0600

    Updated controllers and resource

--- routes/resource.js
+++ routes/resource.js
@@ -12,8 +12,7 @@ router.post('/resource/icecream', icecream_controller.icecream_create_post);
 // DELETE request to delete icecream.
 router.delete('/resource/icecream/:id', icecream_controller.icecream_delete);
 // PUT request to update icecream.
-router.put('/resource/icecream/:id',
-icecream_controller.icecream_update_put);
+router.put('/resource/icecream/:id',icecream_controller.icecream_update_put);
 // GET request for one icecream.
 router.get('/resource/icecream/:id', icecream_controller.icecream_detail);
 // GET request for list of all icecream items.
