32d991105db9c193ac64d22437dbb5ec3861f714
mounishaS545235 <89499369+mounishaS545235@users.noreply.github.com>
2021-11-27 17:11:42 -0600

    Added authentication

--- app.js
+++ app.js
@@ -5,19 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
-passport.use(new LocalStrategy(
-  function (username, password, done) {
-    Account.findOne({ username: username }, function (err, user) {
-      if (err) { return done(err); }
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username.' });
-      }
-      if (!user.validPassword(password)) {
-        return done(null, false, { message: 'Incorrect password.' });
-      }
-      return done(null, user);
-    });
-  }));
 var mongoose = require('mongoose');
 var Icecream = require("./models/icecream");
 
@@ -101,15 +88,22 @@ app.use(require('express-session')({
   saveUninitialized: false
 }));
 app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/icecream', icecreamRouter);
-app.use('/addmods', addmodsRouter);
-app.use('/selector', selectorRouter);
-app.use('/', resourceRouter);
-
+passport.use(new LocalStrategy(
+  function (username, password, done) {
+    Account.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }));
 // passport config
 // Use the existing connection
 // The Account model
@@ -117,6 +111,13 @@ var Account = require('./models/account');
 passport.use(new LocalStrategy(Account.authenticate()));
 passport.serializeUser(Account.serializeUser());
 passport.deserializeUser(Account.deserializeUser());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/icecream', icecreamRouter);
+app.use('/addmods', addmodsRouter);
+app.use('/selector', selectorRouter);
+app.use('/', resourceRouter);
 
 
 // catch 404 and forward to error handler
--- routes/icecream.js
+++ routes/icecream.js
@@ -13,14 +13,27 @@ module.exports = router;
 var express = require('express');
 const icecream_controlers= require('../controllers/icecream');
 var router = express.Router();
+
+// A little function to check if we have an authorized user and continue on
+// or;
+// redirect to login.
+const secured = (req, res, next) => {
+  if (req.user) {
+    return next();
+  }
+  req.session.returnTo = req.originalUrl;
+  res.redirect("/login");
+};
+
+
 /* GET icecreams */
 router.get('/', icecream_controlers.icecream_view_all_Page );
 /* GET detail icecream page */
 router.get('/detail', icecream_controlers.icecream_view_one_Page);
 /* GET create icecream page */
 router.get('/create', icecream_controlers.icecream_create_Page);
-/* GET create update page */
-router.get('/update', icecream_controlers.icecream_update_Page);
-/* GET create icecream page */
+/* GET update icecream page */
+router.get('/update',secured, icecream_controlers.icecream_update_Page);
+/* GET delete icecream page */
 router.get('/delete', icecream_controlers.icecream_delete_Page);
 module.exports = router;
